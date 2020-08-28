e54372be4f30cfdc1f263d6a88532af1dcee2da0
Jose <josexcrono@gmail.com>
2020-08-28 14:10:20 +0100

    First Commit

---
+++ src/main/java/com/guri/goodsManagement/GoodsManagementTechnicalTestApplication.java
@@ -0,0 +1,13 @@
+package com.guri.goodsManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodsManagementTechnicalTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodsManagementTechnicalTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guri/goodsManagement/GoodsManagementTechnicalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guri.goodsManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodsManagementTechnicalTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
